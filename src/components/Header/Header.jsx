f15885c2ca00a5a88c413c74d1a9b42061d6e08a
John Blossom <jblossom.web@gmail.com>
2017-10-14 12:28:23 -0600

    use material design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
 import Logo from '../../assets/logo.svg';
 import classNames from 'classnames/bind';
 import s from './Header.css';
@@ -10,10 +15,16 @@ class Header extends Component {
   render() {
     const { spin, title } = this.props;
     return (
-      <header className={s.root}>
-        <Logo className={cx('logo', { spin })} alt="logo" />
-        <h1 className="Header-title">{ title }</h1>
-      </header>
+      <AppBar position="static" color="default">
+        <Toolbar className={s.root}>
+          <IconButton aria-label="Logo">
+            <Logo className={cx(s.logo, { spin })} alt="logo" />
+          </IconButton>
+          <Typography type="title" color="inherit" className={s.title}>
+            { title }
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
@@ -22,4 +33,4 @@ Header.propTypes = {
   spin: PropTypes.bool
 };
 
-export default Header;
+export default withStyles(s)(Header);
