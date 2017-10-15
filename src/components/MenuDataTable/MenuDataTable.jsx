0d0b3fb2956d08b31d467f26998ea35323864ca1
John Blossom <jblossom.web@gmail.com>
2017-10-15 14:12:56 -0600

    MenuDataTable refactor with state, add column search

--- src/components/MenuDataTable/MenuDataTable.jsx
+++ src/components/MenuDataTable/MenuDataTable.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
 import {
+  FilteringState,
+  LocalFiltering,
+} from '@devexpress/dx-react-grid';
+import {
   Grid,
   TableView,
   TableHeaderRow,
+  TableFilterRow,
 } from '@devexpress/dx-react-grid-material-ui';
 
 import Menu from '../../models/Menu.model';
 import s from './MenuDataTable.css';
 
-const MenuDataTable = props => (
-  <div className={s.root}>
-    <Grid
-      rows={props.menu}
-      columns={[
+class MenuDataTable extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columns: [
         { name: 'name', title: 'Name' },
         { name: 'description', title: 'Description' },
-      ]}
-    >
-      <TableView />
-      <TableHeaderRow />
-    </Grid>
-  </div>
-);
+      ],
+      rows: props.menu,
+    };
+  }
+  render() {
+    const { rows, columns } = this.state;
+
+    return (
+      <Grid
+        rows={rows}
+        columns={columns}
+      >
+        <FilteringState defaultFilters={[]} />
+        <LocalFiltering />
+        <TableView />
+        <TableHeaderRow />
+        <TableFilterRow />
+      </Grid>
+    );
+  }
+}
 
 MenuDataTable.propTypes = Menu.propTypes;
 MenuDataTable.defaultProps = Menu.defaultProps;
