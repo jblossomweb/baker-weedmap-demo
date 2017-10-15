4a7febbe1b679ef51e77098b5cd15e8a54b6c105
John Blossom <jblossom.web@gmail.com>
2017-10-15 10:42:06 -0600

    add some mock dispensaries for stories/tests

---
+++ src/mock/dispensaries.js
@@ -0,0 +1,20 @@
+import images from '../mock/dispensaries/images';
+
+const dispensaries = [
+  {
+    name: 'Native Roots',
+    slug: 'native-roots',
+    city: 'Denver',
+    state: 'Colorado',
+    avatar: images.nativeRoots,
+  },
+  {
+    name: 'Natural Remedies',
+    slug: 'natural-remedies',
+    city: 'Denver',
+    state: 'Colorado',
+    avatar: images.naturalRemedies,
+  },
+];
+
+export default dispensaries;
---
+++ src/mock/dispensaries/images/index.js
@@ -0,0 +1,7 @@
+import nativeRoots from './native-roots.png';
+import naturalRemedies from './natural-remedies.png';
+
+export default {
+  nativeRoots,
+  naturalRemedies,
+};
